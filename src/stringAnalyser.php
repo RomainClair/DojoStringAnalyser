aee207cff933f25f615f0f83bb91101605eb5e82
Romain Clair <romain.clair@wildcodeschool.fr>
2020-11-03 11:26:26 +0100

    First dojo version

--- src/stringAnalyser.php
+++ src/stringAnalyser.php
@@ -8,22 +8,4 @@
 
 declare(strict_types=1);
 
-function countVowels(string $input) : array
-{
-    $results = [
-        "a" => 0,
-        "e" => 0,
-        "i" => 0,
-        "o" => 0,
-        "u" => 0,
-        "y" => 0
-    ];
-    $input = strtolower($input);
-    $size = strlen($input);
-    for ($i = 0; $i < $size; $i++) {
-        if (isset($results[$input[$i]])) {
-            $results[$input[$i]]++;
-        }
-    }
-    return $results;
-}
+// TODO add your countVowels function
